a1298921976712ef586980cfe42f284c2aa478cb
Alexander Spanos <alexisspa9@gmail.com>
2018-04-05 07:31:16 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,8 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit())
 
 
   end
