3525297235efcc3b3ab905faed49f5bd2378956e
Alexander Spanos <alexisspa9@gmail.com>
2018-04-05 07:33:59 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,8 +1,10 @@
 class SchoolClassesController < ApplicationController
   def new
-@school_class = SchoolClass.new
+    @school_class = SchoolClass.new
   end
   def create
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+
 
   end
   def show
